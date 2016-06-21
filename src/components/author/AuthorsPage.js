5497971b72bebef7afeda345ee9dbb8876e778c8
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-06-20 20:12:06 -0400

    add author folder and author page component files

---
+++ src/components/author/AuthorsPage.js
@@ -0,0 +1,5 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
