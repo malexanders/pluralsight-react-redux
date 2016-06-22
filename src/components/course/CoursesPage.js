23ef666751a321245ebf1c925f89954eec97f8b0
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-06-22 18:30:02 -0400

    CoursesPage.js, sort courses array by title alphabetically

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -92,6 +92,14 @@ CoursesPage.propTypes = {
 
 function mapStateToProps(state, ownProps) {
 
+  let sorted = state.courses.sort(function(a, b){
+    if(a.title < b.title)
+      return -1
+    if(a.title > b.title)
+      return 1
+    return 0
+  })
+
   /*_Tip: returns properties that we would like to see exposed on our compoenents*/
   return {
     /*_Tip:
