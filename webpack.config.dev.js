1fca58145bd5f3b4513bc2b4dcd0039b157d5a5d
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-06-23 11:41:06 -0400

    webpack.config.dev.js, add sass-loader to loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 export default {
     debug: true,
@@ -27,7 +28,7 @@ export default {
     module: {
         loaders: [
             {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-            {test: /(\.css)$/, loaders: ['style', 'css']},
+            {test: /(\.scss|\.css)$/, loaders: ['style', 'css', 'sass']},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
             {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
